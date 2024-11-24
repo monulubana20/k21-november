e913a2cec12b0fb3ffeee640193ddecc801a36df
mandeep <mandeep@yahoo.com>
2024-11-24 15:49:56 -0800

    add name to  instance

--- main.tf
+++ main.tf
@@ -22,4 +22,9 @@ resource "aws_instance" "myname" {
     depends_on = [ aws_eip.name ]
   ami           = "ami-01b799c439fd5516a"
   instance_type = "t2.micro"
+
+  tags = {
+
+    Name = "mandeep-instance"
+  }
 }
